e9cad330c9b72adbf0cb336e3ca534ee43308c18
DESKTOP-KVCQHCD\SSAFY <a4121a2@naver.com>
2022-01-18 18:32:33 +0900

    #S06P12A202-65-spring-init

---
+++ backend/momofunding/src/main/java/com/ssafy/momofunding/domain/user/controller/UserApiController.java
@@ -0,0 +1,21 @@
+package com.ssafy.momofunding.domain.user.controller;
+
+import com.ssafy.momofunding.domain.user.dto.UserSaveRequestDto;
+import com.ssafy.momofunding.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class UserApiController {
+    private final UserService userService;
+
+    @PostMapping("/api/user")
+    public int save(@RequestBody UserSaveRequestDto userSaveRequestDto){
+        return userService.save(userSaveRequestDto);
+    }
+
+
+}
