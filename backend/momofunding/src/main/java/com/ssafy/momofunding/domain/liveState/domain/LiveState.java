c9bf400725e8fcc77a4ed070b3cbf36e6b0f3ad7
DESKTOP-KVCQHCD\SSAFY <a4121a2@naver.com>
2022-01-21 15:20:00 +0900

    #S06P12A202-133 :sparkles: Feat: Init live state

---
+++ backend/momofunding/src/main/java/com/ssafy/momofunding/domain/liveState/domain/LiveState.java
@@ -0,0 +1,27 @@
+package com.ssafy.momofunding.domain.liveState.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class LiveState {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length=10)
+    private String stateName;
+
+    @Builder
+    public LiveState(String stateName){
+        this.stateName = stateName;
+    }
+
+}
