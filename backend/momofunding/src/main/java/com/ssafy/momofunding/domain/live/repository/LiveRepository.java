cae57a0c358e4131941125f21c4ea71f3c9d6097
김윤하 <a41212a@naver.com>
2022-01-24 14:47:25 +0900

    #S06P12A202-130 🎨 : Style: Change argument name (L->l)

--- backend/momofunding/src/main/java/com/ssafy/momofunding/domain/live/repository/LiveRepository.java
+++ backend/momofunding/src/main/java/com/ssafy/momofunding/domain/live/repository/LiveRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface LiveRepository extends JpaRepository<Live, Long> {
-    List<Live> findAllByLiveStateId(Long LiveStatId, Sort sort);
+    List<Live> findAllByLiveStateId(Long liveStatId, Sort sort);
     List<Live> findAllByProjectCategoryId(Long projectCategoryId, Sort sort);
 }
