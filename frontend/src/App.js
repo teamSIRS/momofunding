41ee68b35edd94d594b78007a57f8264a37ea149
jiho <hahhaah@naver.com>
2022-01-25 17:36:22 +0900

    #S06P12A202-70 ✨ Feat: Showing List template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ProjectList from "./components/Project/ProjectList";
 import Header from "./container/Header";
 import GlobalStyle from "./shared/global";
 
