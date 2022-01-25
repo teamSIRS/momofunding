e6ef03a6a8de8c94018b5379393cf80bac9baa91
jiho <hahhaah@naver.com>
2022-01-25 15:11:02 +0900

    #S06P12A202-72 ✨ Feat: Find Id component

--- frontend/src/components/Account/FindId.js
+++ frontend/src/components/Account/FindId.js
@@ -5,7 +5,7 @@ const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin: 150px;
+    margin: 100px 150px;
 `;
 
 const Info = styled.div`
@@ -13,7 +13,7 @@ const Info = styled.div`
     height: 70px;
 `;
 
-const InputEmail = styled.input`
+const InputEmail = styled.input.attrs({required:true})`
     width: 470px;
     height: 58px;
     margin-bottom: 15px;
@@ -49,27 +49,54 @@ const NonExist = styled(Exist)`
     color: tomato;
 `;
 
+const InputForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
 function FindId(){
-    let [email, setEmail] = useState(false); ///////////test용
+    let [email, setEmail] = useState('');
+    let [isEmail, setIsEmail] = useState(false); ///////////test용
     let [modal, setModal] = useState(false); ///////////test용
+
+    const checkEmail = (e) => {
+
+        console.log(email);
+        
+        // if(email === '')
+        //     setModal(false);
+        setModal(true);
+
+        if( isEmail ){ //이메일이 존재한다면(if문 안에 비교)
+            return setIsEmail(true);
+        }
+        return null; //이메일이 존재하지 않는다면 setIsEmail(false);
+
+    }
 
     return(
         <Container>
             <Info><p>가입하셨던 이메일 계정을 입력하시면<br/>가입 여부를 확인해드립니다</p></Info>
-            <InputEmail placeholder='이메일 계정'></InputEmail>
-            <Btn onClick={() => {setModal(true)}}>확인</Btn>
+            <InputForm>
+                <InputEmail placeholder='이메일 계정' type='email' onChange={(e) => {setEmail(e.target.value)}}></InputEmail>
+                <Btn onClick={checkEmail} type='button'>확인</Btn>
+            </InputForm>
 
             {
                 modal
                 ? (
                     email
                     ? <Message><Exist>이미 가입한 회원입니다</Exist></Message>
-                    : <Message><NonExist >가입정보가 존재하지 않습니다</NonExist></Message>
+                    : <Message><NonExist>가입정보가 존재하지 않습니다</NonExist></Message>
                  )
                 : null
             }
+
         </Container>
     );
 }
+
+
 
 export default FindId;
