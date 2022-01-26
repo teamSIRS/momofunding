a5a9d939cc8b5e8e53bbb8a979ffd4ac9aa4e519
김윤하 <a41212a@naver.com>
2022-01-26 18:24:35 +0900

    #S06P12A202-181 ✨ : Feat : Add save survey api

---
+++ backend/momofunding/src/main/java/com/ssafy/momofunding/domain/survey/repository/SurveyRepository.java
@@ -0,0 +1,11 @@
+package com.ssafy.momofunding.domain.survey.repository;
+
+import com.ssafy.momofunding.domain.survey.domain.Survey;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SurveyRepository extends JpaRepository<Survey, Long> {
+
+}
