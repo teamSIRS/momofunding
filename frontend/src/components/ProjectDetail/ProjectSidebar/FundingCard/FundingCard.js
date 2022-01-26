3d83f319664a74e88fab83f10ab88d70fcc62644
YoonBaek <royal729@naver.com>
2022-01-27 01:25:33 +0900

    ✨ Feat: Add Project detial sidebar component

---
+++ frontend/src/components/ProjectDetail/ProjectSidebar/FundingCard/FundingCard.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { InfoCard, Text } from "../CreatorCard/styles";
+
+const Card = styled(InfoCard)`
+  height: 410px;
+  align-items: flex-start;
+  justify-content: space-evenly;
+`;
+
+const Title = styled.p`
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+
+const Content = styled.p``;
+
+function FundingCard() {
+  return (
+    <Card>
+      <Text>
+        <Title>상품명</Title>
+        <Content>Jola Mashisseu 2018</Content>
+      </Text>
+      <Text>
+        <Title>남은 기간</Title>
+        <Content>~2/14 34일 남음</Content>
+      </Text>
+      <Text>
+        <Title>달성률</Title>
+        <Content>1083% 달성</Content>
+      </Text>
+      <Text>
+        <Title>펀딩금액</Title>
+        <Content>72,023,456원</Content>
+      </Text>
+    </Card>
+  );
+}
+
+export default FundingCard;
---
+++ frontend/src/components/ProjectDetail/ProjectSidebar/styles.ts
@@ -0,0 +1,6 @@
+import { Col } from "react-bootstrap";
+import styled from "styled-components";
+
+export const Sidebar = styled(Col)`
+  max-width: 360px;
+`;
