bca18d4cdbb3bbd4186b07e591edcfe5992b8252
YoonBaek <royal729@naver.com>
2022-01-27 01:23:20 +0900

    ✨ Feat: Add Project detail banner title

---
+++ frontend/src/components/ProjectDetail/ProjectBanner/BannerCaption/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { MomoColor } from "../../../../shared/global";
+
+export const BannerTitle = styled.h2`
+  color: black;
+  position: absolute;
+  top: 30px;
+  right: 20px;
+`;
+
+export const BannerSubTitle = styled.h3`
+  color: ${MomoColor};
+  position: absolute;
+  top: 70px;
+  right: 20px;
+`;
