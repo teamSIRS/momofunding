d114ffb675630b2782ecc0c761bca5eec17d6f66
Ji-Min Jeong <jeomn9@gmail.com>
2022-01-28 02:35:44 +0900

    #S06P12A202-112 ✨ Feat: Add get-creator-detail-api

--- backend/momofunding/src/main/java/com/ssafy/momofunding/domain/creator/controller/CreatorApiController.java
+++ backend/momofunding/src/main/java/com/ssafy/momofunding/domain/creator/controller/CreatorApiController.java
@@ -1,5 +1,6 @@
 package com.ssafy.momofunding.domain.creator.controller;
 
+import com.ssafy.momofunding.domain.creator.dto.CreatorDetailResponseDto;
 import com.ssafy.momofunding.domain.creator.dto.CreatorUpdateRequestDto;
 import com.ssafy.momofunding.domain.creator.service.CreatorService;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +28,18 @@ public class CreatorApiController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseMap);
         }
         return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @GetMapping("/projects/{projectId}")
+    public ResponseEntity<Object> findCreatorByProjectId(@PathVariable Long projectId){
+        try {
+            CreatorDetailResponseDto creatorDetailResponseDto = creatorService.findCreatorByProjectId(projectId);
+            return ResponseEntity.status(HttpStatus.OK).body(creatorDetailResponseDto);
+        } catch(IllegalArgumentException e){
+            Map<String, Object> responseMap = new HashMap<>();
+            responseMap.put("errorMsg", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseMap);
+        }
     }
     
 }
--- backend/momofunding/src/main/java/com/ssafy/momofunding/domain/creator/service/CreatorService.java
+++ backend/momofunding/src/main/java/com/ssafy/momofunding/domain/creator/service/CreatorService.java
@@ -1,6 +1,7 @@
 package com.ssafy.momofunding.domain.creator.service;
 
 import com.ssafy.momofunding.domain.creator.domain.Creator;
+import com.ssafy.momofunding.domain.creator.dto.CreatorDetailResponseDto;
 import com.ssafy.momofunding.domain.creator.dto.CreatorUpdateRequestDto;
 import com.ssafy.momofunding.domain.creator.repository.CreatorRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,4 +23,9 @@ public class CreatorService {
         creator.updateCreator(creatorUpdateRequestDto);
     }
 
+    public CreatorDetailResponseDto findCreatorByProjectId(Long projectId) {
+        Creator creator = creatorRepository.findByProjectId(projectId)
+                .orElseThrow(()-> new IllegalArgumentException("잘못된 프로젝트 번호입니다:: projectId-"+projectId));
+        return new CreatorDetailResponseDto(creator);
+    }
 }
