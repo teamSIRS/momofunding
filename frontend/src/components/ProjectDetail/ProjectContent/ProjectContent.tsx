f67998fed01e0861f44689d042a45a00de6f6b16
YoonBaek <royal729@naver.com>
2022-01-28 11:58:15 +0900

    ✨ Feat: Add Project detail content outlet

--- frontend/src/components/ProjectDetail/ProjectContent/ProjectContent.tsx
+++ frontend/src/components/ProjectDetail/ProjectContent/ProjectContent.tsx
@@ -1,11 +1,20 @@
-import { Content } from "./styles";
+import { Outlet } from "react-router-dom";
+import {
+  Content,
+  ContentNavbar,
+  ContentNavLink,
+  OutletWrapper,
+} from "./styles";
 
 export const ProjectContent = () => (
   <Content xs={8}>
-    <h1>Content Area</h1>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, odit
-    adipisci? Nisi quia nobis voluptate molestias reiciendis consequuntur
-    dignissimos quod, nulla ipsa aliquid magnam illum neque quidem tempora quasi
-    fuga!
+    <ContentNavbar>
+      <ContentNavLink to={"story"}>스토리</ContentNavLink>
+      <ContentNavLink to={"notices"}>공지사항</ContentNavLink>
+      <ContentNavLink to={"community"}>QnA</ContentNavLink>
+    </ContentNavbar>
+    <OutletWrapper>
+      <Outlet />
+    </OutletWrapper>
   </Content>
 );
--- frontend/src/components/ProjectDetail/ProjectContent/styles.ts
+++ frontend/src/components/ProjectDetail/ProjectContent/styles.ts
@@ -1,6 +1,44 @@
-import { Col } from "react-bootstrap";
+import { Col, Navbar } from "react-bootstrap";
 import styled from "styled-components";
+import { StyledNavLink } from "../../../container/Header/Navbar/Menus/Menu/styles";
+import { MomoColor, MomoWeakColor } from "../../../shared/global";
 
 export const Content = styled(Col)`
   max-width: 720px;
+`;
+
+export const ContentNavbar = styled(Navbar)`
+  background-color: transparent;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin: auto;
+  font-size: 18px;
+  box-shadow: 0px 30px 20px -30px ${MomoColor};
+  margin-bottom: 12px;
+`;
+
+export const ContentNavLink = styled(StyledNavLink)`
+  position: relative;
+  color: ${MomoColor};
+  &:after {
+    content: "";
+    position: absolute;
+    background-color: ${MomoWeakColor};
+    height: 6px;
+    width: 0;
+    left: -100%;
+    bottom: -27px;
+    transition: 0.8s;
+    border-radius: 5px;
+  }
+  &:hover:after {
+    width: 300%;
+  }
+`;
+
+export const OutletWrapper = styled.div`
+  padding: 8px;
 `;
