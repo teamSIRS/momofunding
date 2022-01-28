6dd2e52ae9aeea033cadf7ebd148bcec4635973a
YoonBaek <royal729@naver.com>
2022-01-28 13:16:06 +0900

    ✨ Feat: Add project detail qna card

--- frontend/src/components/ProjectDetail/ProjectContent/ProjectCommunity/PjCommunityQnA/PjCommunityQnACard/PJCommunityQnACard.tsx
+++ frontend/src/components/ProjectDetail/ProjectContent/ProjectCommunity/PjCommunityQnA/PjCommunityQnACard/PJCommunityQnACard.tsx
@@ -1,1 +1,27 @@
-export const PjCommunityQnACard = () => {};
+import { Card } from "react-bootstrap";
+import { ProfileImg, QnACard } from "./styles";
+
+type QnACardProps = {
+  username: string;
+  content: string;
+  date: string;
+};
+
+export const PjCommunityQnACard = ({
+  username,
+  content,
+  date,
+}: QnACardProps) => {
+  return (
+    <QnACard>
+      <ProfileImg
+        width="0px"
+        src="https://media.istockphoto.com/vectors/male-profile-flat-blue-simple-icon-with-long-shadow-vector-id522855255?k=20&m=522855255&s=612x612&w=0&h=fLLvwEbgOmSzk1_jQ0MgDATEVcVOh_kqEe0rqi7aM5A="
+      ></ProfileImg>
+      <Card.Title>{username}</Card.Title>
+      <Card.Text>{content}</Card.Text>
+      <Card.Text>{date}</Card.Text>
+      <hr />
+    </QnACard>
+  );
+};
---
+++ frontend/src/components/ProjectDetail/ProjectContent/ProjectCommunity/PjCommunityQnA/PjCommunityQnACard/styles.ts
@@ -0,0 +1,12 @@
+import { Card } from "react-bootstrap";
+import styled from "styled-components";
+
+export const QnACard = styled(Card)`
+  padding: 8px 0px;
+  border: 0px;
+`;
+
+export const ProfileImg = styled(Card.Img)`
+  width: 70px;
+  border-radius: 35px;
+`;
