7a199d236b09139a0638648ad37751c47036a4fc
YoonBaek <royal729@naver.com>
2022-01-28 11:58:45 +0900

    ✨ Feat: Add Project detail content community outlet

---
+++ frontend/src/components/ProjectDetail/ProjectContent/ProjectCommunity/ProjectCommunity.tsx
@@ -0,0 +1,10 @@
+import PJCommunityQnA from "./PjCommunityQnA";
+import PjCommunityInput from "./PjCommunityQnA/PjCommunityInput";
+
+export const ProjectCommunity = () => {
+  return (
+    <div>
+      <PJCommunityQnA />
+    </div>
+  );
+};
