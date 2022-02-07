5b7e6a353acecd6a1123971446a7b9300a32b69a
jiho <hahhaah@naver.com>
2022-02-07 22:37:17 +0900

    #S06P12A202-215 ✨ Feat: showing funding list

---
+++ frontend/src/components/Project/Project.styled.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const ListNav = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 20px auto 0 auto;
+  width: 80%;
+  span {
+    margin: 10px;
+  }
+`;
+
+export const Category = styled.div`
+  margin: auto 0;
+`;
+
+export const Search = styled.div`
+  display: flex;
+  margin: 10px 10px;
+  input {
+    text-align: 30px;
+    height: 30px;
+    border: 2px solid black;
+    border-radius: 17px;
+    margin-left: 15px;
+    padding-left: 15px;
+  }
+  svg {
+    width: 25px;
+    height: 25px;
+    margin: 0 30px 0 10px;
+  }
+  p {
+    margin: 3px 10px;
+  }
+`;
+
+export const Bar = styled.hr`
+  display: flex;
+  margin: 0 auto;
+  width: 80%;
+  size: 10px;
+`;
+
+export const ListFilter = styled.p`
+`;
+
+export const ListFilterSelected = styled.p`
+  font-weight: bold;
+`;
+
+export const ListCenter = styled.div`  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
--- frontend/src/components/Project/ProjectList.js
+++ frontend/src/components/Project/ProjectList.js
@@ -1,19 +1,121 @@
 // // 카드로 여러개 나열되어 있는 페이지
+import axios from 'axios';
+import { useEffect, useState } from "react";
 import { Col, Container, Row } from 'react-bootstrap';
-import ProjectNav from "./ProjectNav";
+import HomeBanners from "../Home/HomeBanners";
 import ProjectCard from "./ProjectCard";
-import styled from 'styled-components';
-
-const ListCenter = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+import { ListNav, Category, Search, Bar, ListFilter, ListFilterSelected, ListCenter } from './Project.styled';
 
 function ProjectList(){
+  const baseUrl = "http://localhost:8080";
+  const [isPop, setIsPop] = useState(false);
+  const [isDate, setIsDate] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const showDateList = async() => { //default
+    await axios({
+      url:`/projects?sort=date`,
+      method:"get",
+      baseURL: baseUrl,
+    })
+    .then((response)=>{
+      console.log(response.data);
+      console.log('최신순');
+      setIsDate(true);
+      setIsPop(false);
+    })
+    .catch((err) =>{
+      console.log(err);
+    })
+  }
+  
+  const showPopList = async() => {
+    await axios({
+      url:`/projects?sort=popularity`,
+      method:"get",
+      baseURL: baseUrl,
+    })
+    .then((response)=>{
+      console.log(response.data);
+      console.log('인기순');
+      setIsPop(true);
+      setIsDate(false);
+    })
+    .catch((err) =>{
+      console.log(err);
+    })
+  }
+
+  useEffect(()=>{
+    showDateList();
+  }, []);
+
+  const categories = ['0', '1', '2', '3', '4'];
+  const [selected, setSelected] = useState("");
+
+  const handleSelect = (e) =>{
+    console.log(e.target.value);
+    setSelected(e.target.value);
+  }
+
+
   return(
     <>
-      <ProjectNav />
+      <HomeBanners />
+      <ListNav>
+        <Category>
+          <span id="category">카테고리</span>
+          <select onChange={handleSelect} value={selected}>
+            {categories.map((item) =>(
+              <option value={item} key={item}>
+                {item}
+              </option>
+            ))}
+            </select>
+          {/* </select>
+          <select name="category">
+            <option value="art">예술</option>
+            <option value="food">음식</option>
+            <option value="donation">기부</option>
+          </select> */}
+        </Category>
+
+        <Search>
+          <input
+            type="text"
+            onChange={(e) => {
+              setSearch(e.target.value);
+            }}
+          />
+          <svg
+            onClick={() => {
+              console.log(search, '검색');
+            }}
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-search"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+          </svg>
+
+          {
+            isPop
+            ?(<ListFilterSelected onClick={showPopList}>인기순</ListFilterSelected>)
+            :(<ListFilter onClick={showPopList}>인기순</ListFilter>)
+          }
+          {
+            isDate
+            ?(<ListFilterSelected onClick={showDateList}>최신순</ListFilterSelected>)
+            :(<ListFilter onClick={showDateList}>최신순</ListFilter>)
+          }
+          {/* <ListFilter onClick={showPopList}>인기순</ListFilter> */}
+          {/* <ListFilter onClick={showDateList}>최신순</ListFilter> */}
+        </Search>
+      </ListNav>
+      <Bar />
 
       <Container>
         <ListCenter>
