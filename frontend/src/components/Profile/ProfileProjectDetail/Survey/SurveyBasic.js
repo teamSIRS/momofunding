5511460fe8c753ba08c7571cb039f28f9dc153c5
YoonBaek <royal729@naver.com>
2022-02-07 10:56:58 +0900

    Merge conflict reslove

--- frontend/src/components/Profile/ProfileProjectDetail/Survey/SurveyBasic.js
+++ frontend/src/components/Profile/ProfileProjectDetail/Survey/SurveyBasic.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:frontend/src/components/Profile/ProfileMyPage/Survey/SurveyBasic.js
 import React from "react";
 import {
   Body,
@@ -18,21 +17,6 @@ function SurveyBasic({ survey }) {
       <EditIcon />
     </Body>
   );
-=======
-import React from 'react';
-import { Body, Container, SurveyTitle, SurveyResult, EditIcon } from '../Survey/Survey.styled';
-
-function SurveyBasic({survey}){
-    return(
-        <Body>
-            <Container>
-                <SurveyTitle>{survey.title}</SurveyTitle>
-                <SurveyResult>결과 확인</SurveyResult>
-            </Container>
-            <EditIcon/>
-        </Body>
-    );
->>>>>>> 5a3354c0ca600109ac4d020ef404ab98474aeada:frontend/src/components/Profile/ProfileProjectDetail/Survey/SurveyBasic.js
 }
 
 export default SurveyBasic;
