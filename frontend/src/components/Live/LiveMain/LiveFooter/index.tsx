da75bbb484312a1862fc253f4cf3450f865d34b1
YoonBaek <royal729@naver.com>
2022-02-08 14:35:59 +0900

    #S06P12A202-33 ✨ Feat: Add Live Footer controllbtn

---
+++ frontend/src/components/Live/LiveMain/LiveFooter/index.tsx
@@ -0,0 +1,110 @@
+import { Link } from "react-router-dom";
+import { ChatIcon, LiveBtnRoundDanger } from "../Chat/styles";
+import {
+  exitOutline,
+  newspaperOutline,
+  chatboxEllipsesOutline,
+  videocamOutline,
+  micOutline,
+  videocamOffOutline,
+  micOffOutline,
+  volumeHighOutline,
+  volumeMuteOutline,
+} from "ionicons/icons";
+import {
+  ControlBtn,
+  LiveController,
+  LiveFooterWrapper,
+  LiveToggle,
+  ToggleBtn,
+} from "../styles";
+import { useRecoilState } from "recoil";
+import {
+  audioState,
+  authorizationState,
+  camState,
+  micState,
+  sidebarState,
+  submitState,
+} from "../LiveMain";
+import { MouseEventHandler } from "react";
+
+const LiveFooter = () => {
+  const [show, setShow] = useRecoilState(sidebarState);
+  const [isCreator, _] = useRecoilState(authorizationState);
+  const [camOn, setCam] = useRecoilState(camState);
+  const [micOn, setMic] = useRecoilState(micState);
+  const [audioOn, setAudio] = useRecoilState(audioState);
+  const [surveyState, __] = useRecoilState(submitState);
+
+  const onClick: MouseEventHandler<HTMLDivElement> = () => {
+    setShow((now: boolean) => !now);
+  };
+
+  const onCamClick: MouseEventHandler<HTMLDivElement> = () => {
+    setCam((now: boolean) => !now);
+  };
+
+  const onMicClick: MouseEventHandler<HTMLDivElement> = () => {
+    setMic((now: boolean) => !now);
+  };
+
+  const onAudioClick: MouseEventHandler<HTMLDivElement> = () => {
+    setAudio((now: boolean) => !now);
+  };
+
+  const surveyDone = () => {
+    if (!surveyState) {
+      return "survey-blink";
+    }
+    return "survey";
+  };
+
+  return (
+    <LiveFooterWrapper>
+      <LiveController>
+        {isCreator ? (
+          <>
+            <ControlBtn onClick={onCamClick} className={camOn ? "on" : "off"}>
+              {camOn ? (
+                <ChatIcon icon={videocamOutline} className="survey" />
+              ) : (
+                <ChatIcon icon={videocamOffOutline} />
+              )}
+            </ControlBtn>
+            <ControlBtn onClick={onMicClick} className={micOn ? "on" : "off"}>
+              {micOn ? (
+                <ChatIcon icon={micOutline} className="survey" />
+              ) : (
+                <ChatIcon icon={micOffOutline} />
+              )}
+            </ControlBtn>
+          </>
+        ) : (
+          <Link to="/">
+            <LiveBtnRoundDanger>
+              <ChatIcon icon={exitOutline} />
+            </LiveBtnRoundDanger>
+          </Link>
+        )}
+        <ControlBtn onClick={onAudioClick} className={audioOn ? "on" : "off"}>
+          {audioOn ? (
+            <ChatIcon icon={volumeHighOutline} className="survey" />
+          ) : (
+            <ChatIcon icon={volumeMuteOutline} />
+          )}
+        </ControlBtn>
+      </LiveController>
+      <LiveToggle className={show ? "" : "survey"} onClick={onClick}>
+        <ToggleBtn className={show ? "" : surveyDone()}>
+          <ChatIcon
+            icon={show ? newspaperOutline : chatboxEllipsesOutline}
+            className={show ? "" : "survey"}
+          />
+        </ToggleBtn>
+      </LiveToggle>
+    </LiveFooterWrapper>
+  );
+};
+
+export default LiveFooter;
