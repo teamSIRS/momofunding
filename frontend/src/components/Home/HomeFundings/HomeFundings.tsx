c0fd8f55bea7539612b6181000b1134c61d939aa
Ji-Min Jeong <jeomn9@gmail.com>
2022-02-13 04:35:09 +0900

    ✨ Feat: set main page project list

--- frontend/src/components/Home/HomeFundings/HomeFundings.tsx
+++ frontend/src/components/Home/HomeFundings/HomeFundings.tsx
@@ -1,50 +1,57 @@
+import axios from 'axios';
+import { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import HomeFundingCard from "./HomeFundingCard";
+import { baseUrl } from '../../../App';
 
 export const HomeFundings = () => {
-  const apiLarge = {
-    title: "테스트용 타이틀1",
-    progress: "1086% 초과 달성!",
-    imgSrc:
-      "https://tumblbug-pci.imgix.net/932499bdfd401c73ae81db5270ea5a8a834f7a87/361ce6f682af56e5421e0a86d44a6fe37d799ac0/cd2062d2e9aa90d157b3fe7a5bace351c00d8b1f/a6fd36b0-e132-4807-a98f-9faa0c5fae03.png?auto=format%2Ccompress&fit=crop&h=465&lossless=true&w=620&s=af8e4baf5cbe5cfd092d10a7d3396b41",
-    projectPath: "projects/1",
-  };
+  const [projectLarge, setProjectLarge] = useState<any>("");
+  const [projectSmall, setProjectSmall] = useState<any[]>([""]);
 
-  const apis = [
-    {
-      title: "테스트용 타이틀1",
-      imgSrc:
-        "https://tumblbug-pci.imgix.net/932499bdfd401c73ae81db5270ea5a8a834f7a87/361ce6f682af56e5421e0a86d44a6fe37d799ac0/cd2062d2e9aa90d157b3fe7a5bace351c00d8b1f/a6fd36b0-e132-4807-a98f-9faa0c5fae03.png?auto=format%2Ccompress&fit=crop&h=465&lossless=true&w=620&s=af8e4baf5cbe5cfd092d10a7d3396b41",
-      projectPath: "projects/2",
-    },
-    {
-      title: "테스트용 타이틀2",
-      imgSrc:
-        "https://tumblbug-pci.imgix.net/932499bdfd401c73ae81db5270ea5a8a834f7a87/361ce6f682af56e5421e0a86d44a6fe37d799ac0/cd2062d2e9aa90d157b3fe7a5bace351c00d8b1f/a6fd36b0-e132-4807-a98f-9faa0c5fae03.png?auto=format%2Ccompress&fit=crop&h=465&lossless=true&w=620&s=af8e4baf5cbe5cfd092d10a7d3396b41",
-      projectPath: "projects/3",
-    },
-  ];
+  const getProjects = async() => {
+    await axios({
+      url:`/projects/search?order=popularity`,
+      method:"get",
+      baseURL: baseUrl,
+    })
+    .then((response)=>{
+      setProjectLarge(response.data[0]);
+      setProjectSmall(response.data.slice(1, 3));      
+    })
+    .catch((err) =>{
+      console.log(err);
+    })
+  }
+
+  useEffect(()=>{
+    getProjects();
+  }, []);
+
   return (
     <Container>
       <h2>인기 펀딩 목록</h2>
       <Row md={1} lg={2} className="g-4">
         <Col>
           <HomeFundingCard
-            title={apiLarge.title}
-            imgSrc={apiLarge.imgSrc}
-            progress={apiLarge.progress}
-            projectPath={apiLarge.projectPath}
+            progress={projectLarge.popularity+"% 진행 중!"}
+            title={projectLarge.projectName}
+            imgSrc={projectLarge.subImageUrl}
+            projectPath={"projects/"+projectLarge.id}
+            isLive={projectLarge.isLivePlaying}
             height={"300px"}
             width={"400px"}
           ></HomeFundingCard>
+          
+
         </Col>
         <Col>
-          {apis.map((api, idx) => (
+          {projectSmall.map((project, idx) => (
             <HomeFundingCard
               key={idx}
-              title={api.title}
-              imgSrc={api.imgSrc}
-              projectPath={api.projectPath}
+              title={project.projectName}
+              imgSrc={project.subImageUrl}
+              projectPath={"projects/"+project.id}
+              isLive={project.isLivePlaying}
               height={"140px"}
               width={"400px"}
             />
