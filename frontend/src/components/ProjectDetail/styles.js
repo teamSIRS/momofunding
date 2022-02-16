a94cbf1f3c43af5c9da401b8b6e490e50c77e394
limgeonho <ghlim909@gmail.com>
2022-02-16 17:00:20 +0900

    #S06P12A202 Fix: background color

--- frontend/src/components/ProjectDetail/styles.js
+++ frontend/src/components/ProjectDetail/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const FundingContainer = styled.div`
   display: flex;
   flex-direction: column;
   position: relative;
