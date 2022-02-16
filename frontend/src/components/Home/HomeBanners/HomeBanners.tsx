62bb9eb3ad8fdd345ad56a64aa55fc64f7c1757e
limgeonho <ghlim909@gmail.com>
2022-02-16 18:24:28 +0900

    #S06P12A202 Fix: homebanner onClick

--- frontend/src/components/Home/HomeBanners/HomeBanners.tsx
+++ frontend/src/components/Home/HomeBanners/HomeBanners.tsx
@@ -1,28 +1,40 @@
 import { Carousel } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import HomeBanner from "./HomeBanner";
 
 export const HomeBanners = () => {
   const apis = [
     {
-      title: "당신의 바리스타를 구독하세요",
+      title: "가구 하나로 집안의 전체 분위기를 바꿔보세요.",
       imgSrc:
-        "https://cdn.pixabay.com/photo/2020/04/17/12/49/barista-5055060_960_720.jpg",
-      slogan: "101가지 최상의 떼루아를 머금은 커피를 매일 엄선해 보내드립니다.",
+        "https://cdn.pixabay.com/photo/2014/08/11/21/39/wall-416060_960_720.jpg",
+      slogan: "장인이 직접 만든 가구로 나만의 인테리어를 완성할 수 있습니다.",
+      id: "1",
     },
     {
-      title: "국가기능장 선생님 컬렉션",
+      title: "자유자재로 색상 변경이 가능한 스마트 조명",
       imgSrc:
-        "https://cdn.pixabay.com/photo/2018/05/12/19/00/drechsler-3394311_960_720.jpg",
-      slogan: "Just Buy It",
+        "https://cdn.pixabay.com/photo/2016/11/13/21/08/light-bulbs-1822058_960_720.jpg",
+      slogan: "조명 하나로 바뀌는 분위기를 경험하세요.",
+      id: "2",
     },
     {
-      title: "누구나 사용할 수 있는 순한 면수건",
+      title: "뽀송뽀송하고 부드러운 양말",
       imgSrc:
-        "https://cdn.pixabay.com/photo/2019/10/17/11/01/towels-4556644_960_720.jpg",
-      slogan: "피부가 약하고 예민한 누구나 사용할 수 있는 순한 수건입니다!",
+        "https://cdn.pixabay.com/photo/2016/07/04/04/29/datang-1495918_960_720.jpg",
+      slogan: "발에 땀이 많은 누구나 사용할 수 있는 양말입니다!",
+      id: "3",
     },
   ];
+
+  const navigate = useNavigate();
+  const GoToPjt = (idx: string) => {
+    console.log("dd");
+    console.log(idx);
+  };
+
+  const BannerCarouselItem = styled(Carousel.Item)``;
 
   const BannerCarousel = styled(Carousel)`
     min-width: 820px;
@@ -30,14 +42,14 @@ export const HomeBanners = () => {
   return (
     <BannerCarousel>
       {apis.map((api, idx) => (
-        <Carousel.Item key={idx}>
+        <BannerCarouselItem key={idx} onClick={GoToPjt(api.id)}>
           <HomeBanner
             index={idx}
             title={api.title}
             imgSrc={api.imgSrc}
             slogan={api.slogan}
           />
-        </Carousel.Item>
+        </BannerCarouselItem>
       ))}
     </BannerCarousel>
   );
