2bbdd35fb7abc1cb5564d7c317ba215b8da21528
limgeonho <ghlim909@gmail.com>
2022-02-17 16:55:29 +0900

    #S06P12A202 Fix: Home banner image

--- frontend/src/components/Home/HomeBanners/HomeBanner/HomeBanner.tsx
+++ frontend/src/components/Home/HomeBanners/HomeBanner/HomeBanner.tsx
@@ -1,4 +1,5 @@
 import { Carousel } from "react-bootstrap";
+import styled from "styled-components";
 import { ImageForBg } from "../../../Live/LiveMain/Chat/styles";
 import { BadgeContainer } from "../../HomeFundings/HomeFundingCard/styles";
 import { BannerBox, StyledImg } from "./styles";
@@ -16,12 +17,30 @@ export const HomeBanner = ({
   title,
   slogan,
 }: HomeBannerProps) => {
+  const MessageTitle = styled.div`
+    font-size: 25px;
+    color: white;
+    position: absolute;
+    top: 30%;
+    left: 20%;
+  `;
+
+  const Message = styled(MessageTitle)`
+    font-size: 35px;
+    font-weight: bold;
+    top: 35%;
+    margin-top: 20px;
+  `;
+
   return (
     <BannerBox>
       <StyledImg src={imgSrc} alt={index.toString()} />
-      {/* <BadgeContainer bottom="18px" left="30px">
-        <LiveBadge color="red" content={"라이브"} />
-      </BadgeContainer> */}
+
+      <MessageTitle>펀딩의 첫 시작</MessageTitle>
+      <Message as={"p"}>
+        성공적인 첫 펀딩을 위해, <br />
+        모모펀딩으로 시작하세요.
+      </Message>
       <Carousel.Caption>
         <h3>{title}</h3>
         <p>{slogan}</p>
