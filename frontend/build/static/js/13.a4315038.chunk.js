8070c32433f85d384033283aec2b74c976fdc583
Ji-Min Jeong <jeomn9@gmail.com>
2022-02-18 10:56:34 +0900

    # 🐛 Fix: Delete build folder in frontend

--- frontend/build/static/js/13.a4315038.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[13],{13:function(n,e,r){r.r(e),r.d(e,{ion_modal:function(){return t.M}});var t=r(9722);r(4039),r(323),r(7368),r(236),r(1045),r(9022),r(8030),r(9691),r(7481),r(2091),r(3314),r(5716)}}]);
