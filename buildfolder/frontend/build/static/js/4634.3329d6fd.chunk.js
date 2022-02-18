4e77971bc0989a317e22ef21124f191adfbbe09c
Ji-Min Jeong <jeomn9@gmail.com>
2022-02-18 10:38:38 +0900

    ✨ Feat: Upload front build file

---
+++ buildfolder/frontend/build/static/js/4634.3329d6fd.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[4634],{4634:function(n,e,r){r.r(e),r.d(e,{ion_popover:function(){return o.P}});var o=r(9722);r(4039),r(323),r(7368),r(236),r(1045),r(9022),r(8030),r(9691),r(7481),r(2091),r(3314),r(5716)}}]);
